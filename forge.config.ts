8cf6ffc78afd891ea774309aacf70f1e8939a9b4
hrishi045 <git@hrishi.one>
2026-03-06 17:46:40 +0530

    Start work on windows packaging

--- forge.config.ts
+++ forge.config.ts
@@ -1,5 +1,5 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
-// import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 // import { MakerZIP } from "@electron-forge/maker-zip";
 // import { MakerDeb } from "@electron-forge/maker-deb";
 // import { MakerRpm } from "@electron-forge/maker-rpm";
@@ -16,7 +16,7 @@ const config: ForgeConfig = {
   },
   rebuildConfig: {},
   makers: [
-    // new MakerSquirrel({}),
+    new MakerSquirrel({}),
     // new MakerZIP({}, ["darwin"]),
     // new MakerRpm({}),
     // new MakerDeb({}),
